a988955ab772dcdbdc29432c1b89abacde489b37
itsme-brijeshrai <brijeshrai9820@gmail.com>
2023-10-15 11:09:07 +0530

    defining movie struct and api methods

--- main.go
+++ main.go
@@ -9,3 +9,30 @@ import(
 	"strconv"
 	"github.com/gorilla/mux"
 )
+
+type Movie struct{
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct{
+	firstName string `json:"firstName"`
+	lastName string `json:"lastName"`
+}
+
+var movies []Movie
+
+func main(){
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies/create", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movie/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Print("Starting server at port 8000 \n")
+	log.Fatal(http.ListenAndServe(":8000",r))
+}
